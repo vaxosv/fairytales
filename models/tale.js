c10abe9b2ac979282700df4f9bac699be837c139
Beka Tsalani <beqacalani@gmail.com>
2018-05-19 00:42:18 +0400

    added tale module

---
+++ models/tale.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose');
+
+let TaleSchema = mongoose.Schema({
+    title: String,
+    content: String,
+    categorieIds: [],
+    createDate: {type: Date, default: new Date()}
+});
+
+module.exports = mongoose.model('tales', TaleSchema);
+
+module.exports.addNew = function (data, cb) {
+    let Tale = this;
+    let newTale = new Tale();
+
+    newTale.title = data.title;
+    newTale.content = data.content;
+    newTale.categorieIds = data.categorieIds;
+
+    newTale.save(cb);
+};
+
+module.exports.getAll = function (cb) {
+    let Tale = this;
+    Tale.find(cb);
+};
+
+module.exports.getById = function (id, cb) {
+    let Tale = this;
+    Tale.findById(id,cb);
+};
+
+module.exports.remove = function (id, cb) {
+    let Tale = this;
+    Tale.findByIdAndRemove(id,cb);
+};
